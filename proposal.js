7cdd8d5d5e986d418d3c09d5d91bd4dd11c6d031
virtualmoneybd <mohammadshahrozislam@gmail.com>
2026-05-07 23:07:28 +0600

    Add acceptLove function for sharing messages

---
+++ proposal.js
@@ -0,0 +1,15 @@
+function acceptLove(){
+
+alert("Alhamdulillah ❤️");
+
+window.open(
+"https://wa.me/8801404579502?text=Alhamdulillah%20❤️",
+"_blank"
+);
+
+window.open(
+"https://mail.google.com/mail/?view=cm&fs=1&to=mohammadshahrozislam@gmail.com&su=She%20Accepted%20❤️&body=Alhamdulillah%20❤️",
+"_blank"
+);
+
+}
